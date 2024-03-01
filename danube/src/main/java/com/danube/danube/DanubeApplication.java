9764c9091880f9d09fe09084647b8013151db701
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-01 20:27:17 +0100

    Initialize spring project

---
+++ danube/src/main/java/com/danube/danube/DanubeApplication.java
@@ -0,0 +1,13 @@
+package com.danube.danube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanubeApplication.class, args);
+	}
+
+}
