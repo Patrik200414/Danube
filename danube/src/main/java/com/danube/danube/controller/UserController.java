ae74b365889ea2fe3a878a258d8688c26db3432d
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-02 13:38:01 +0100

    Create UserRegistrationDTO

---
+++ danube/src/main/java/com/danube/danube/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.danube.danube.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @PostMapping("/registration")
+    public ResponseEntity<?> registration(){
+
+    }
+}
---
+++ danube/src/main/java/com/danube/danube/model/dto/UserRegistrationDTO.java
@@ -0,0 +1,4 @@
+package com.danube.danube.model.dto;
+
+public record UserRegistrationDTO(String firstName, String lastName, String email, String password) {
+}
