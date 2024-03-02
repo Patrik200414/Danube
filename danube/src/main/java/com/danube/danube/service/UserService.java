74903131e2dce46bbd03df7f9355c1e19a2fc8f0
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-02 15:19:39 +0100

    Validate user's information

--- danube/src/main/java/com/danube/danube/service/UserService.java
+++ danube/src/main/java/com/danube/danube/service/UserService.java
@@ -1,5 +1,8 @@
 package com.danube.danube.service;
 
+import com.danube.danube.custom_exception.InputTooShortException;
+import com.danube.danube.custom_exception.InvalidEmailFormatException;
+import com.danube.danube.custom_exception.RegistrationFieldNullException;
 import com.danube.danube.model.dto.UserRegistrationDTO;
 import com.danube.danube.model.user.UserEntity;
 import com.danube.danube.repository.user.UserRepository;
@@ -20,7 +23,30 @@ public class UserService {
     }
 
     public void saveUser(UserRegistrationDTO userRegistrationDTO){
+        registrationValidator(userRegistrationDTO);
         UserEntity user = Converter.convertUserRegistrationDTOToUserEntity(userRegistrationDTO, passwordEncoder);
         userRepository.save(user);
+    }
+
+    private void registrationValidator(UserRegistrationDTO userRegistrationDTO){
+        if(userRegistrationDTO.email() == null){
+            throw new RegistrationFieldNullException("email");
+        } else if(userRegistrationDTO.firstName() == null){
+            throw new RegistrationFieldNullException("first name");
+        } else if(userRegistrationDTO.lastName() == null){
+            throw new RegistrationFieldNullException("last name");
+        } else if(userRegistrationDTO.password() == null){
+            throw new RegistrationFieldNullException("password");
+        }
+
+        if(userRegistrationDTO.firstName().length() < 2){
+            throw new InputTooShortException("first name");
+        } else if(userRegistrationDTO.password().length() < 2){
+            throw new InputTooShortException("password");
+        } else if(userRegistrationDTO.lastName().length() < 2){
+            throw new InputTooShortException("last name");
+        } else if(!userRegistrationDTO.email().matches("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}")){
+            throw new InvalidEmailFormatException();
+        }
     }
 }
