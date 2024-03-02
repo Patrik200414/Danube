c33fdb0f6db643cb868657acc23b6ebdd8023dff
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-02 13:48:35 +0100

    Implement saveUser in UserService

--- danube/src/main/java/com/danube/danube/service/UserService.java
+++ danube/src/main/java/com/danube/danube/service/UserService.java
@@ -1,2 +1,26 @@
-package com.danube.danube.service;public class UserService {
+package com.danube.danube.service;
+
+import com.danube.danube.model.dto.UserRegistrationDTO;
+import com.danube.danube.model.user.UserEntity;
+import com.danube.danube.repository.user.UserRepository;
+import com.danube.danube.service.utility.Converter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void saveUser(UserRegistrationDTO userRegistrationDTO){
+        UserEntity user = Converter.convertUserRegistrationDTOToUserEntity(userRegistrationDTO, passwordEncoder);
+        userRepository.save(user);
+    }
 }
