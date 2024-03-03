30406a123f7f5dfb670d153fca5822e43d73fd15
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-03 14:51:04 +0100

    Refactor login endpoint

--- danube/src/main/java/com/danube/danube/controller/UserController.java
+++ danube/src/main/java/com/danube/danube/controller/UserController.java
@@ -28,15 +28,11 @@ public class UserController {
 
     private final UserService userService;
     private final Advice controllerAdvice;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
 
     @Autowired
     public UserController(UserService userService, Advice controllerAdvice, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.userService = userService;
         this.controllerAdvice = controllerAdvice;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtils = jwtUtils;
     }
 
     @PostMapping("/registration")
@@ -52,19 +48,8 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserLoginDTO userLoginDTO){
         try{
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password())
-            );
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            String jwt = jwtUtils.generateJwtToken(authentication);
-
-            User userDetails = (User) authentication.getPrincipal();
-            List<String> roles = userDetails.getAuthorities().stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .toList();
-
-            return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), roles));
+            JwtResponse jwtResponse = userService.loginUser(userLoginDTO);
+            return ResponseEntity.ok(jwtResponse);
         } catch (Exception e){
             return controllerAdvice.handleException(e);
         }
--- danube/src/main/java/com/danube/danube/service/UserService.java
+++ danube/src/main/java/com/danube/danube/service/UserService.java
@@ -1,33 +1,67 @@
 package com.danube.danube.service;
 
+import com.danube.danube.custom_exception.EmailNotFoundException;
 import com.danube.danube.custom_exception.InputTooShortException;
 import com.danube.danube.custom_exception.InvalidEmailFormatException;
 import com.danube.danube.custom_exception.RegistrationFieldNullException;
+import com.danube.danube.model.dto.JwtResponse;
+import com.danube.danube.model.dto.UserLoginDTO;
 import com.danube.danube.model.dto.UserRegistrationDTO;
 import com.danube.danube.model.user.UserEntity;
 import com.danube.danube.repository.user.UserRepository;
+import com.danube.danube.security.jwt.JwtUtils;
 import com.danube.danube.service.utility.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
     }
 
     public void saveUser(UserRegistrationDTO userRegistrationDTO){
         registrationValidator(userRegistrationDTO);
         UserEntity user = Converter.convertUserRegistrationDTOToUserEntity(userRegistrationDTO, passwordEncoder);
         userRepository.save(user);
+    }
+
+    public JwtResponse loginUser(UserLoginDTO userLoginDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        User userDetails = (User) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        UserEntity user = userRepository.findByEmail(userLoginDTO.email()).orElseThrow(() -> new EmailNotFoundException(userLoginDTO.email()));
+
+        return new JwtResponse(jwt, user.getFirstName(), roles);
     }
 
     private void registrationValidator(UserRegistrationDTO userRegistrationDTO){
