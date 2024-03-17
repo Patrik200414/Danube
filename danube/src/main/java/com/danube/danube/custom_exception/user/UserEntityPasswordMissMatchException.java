1b49a0e641a6cec9e244eae25f8a89628f994d33
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-17 16:21:32 +0100

    Create custom UserEntityPasswordMissMatchException

--- danube/src/main/java/com/danube/danube/custom_exception/user/UserEntityPasswordMissMatchException.java
+++ danube/src/main/java/com/danube/danube/custom_exception/user/UserEntityPasswordMissMatchException.java
@@ -1,2 +1,8 @@
-package com.danube.danube.custom_exception.user;public class UserEntityPasswordMissMatch {
+package com.danube.danube.custom_exception.user;
+
+public class UserEntityPasswordMissMatchException extends RuntimeException{
+
+    public UserEntityPasswordMissMatchException() {
+        super("Unmatching password for the given usr!");
+    }
 }
