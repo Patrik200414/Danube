9b4adab478e0003406593faeb485c5de02d6e0bd
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-18 09:35:27 +0100

    Create Verification page

--- client/src/page/Verification.jsx
+++ client/src/page/Verification.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import UserAccountInformation from "../component/UserAccountInformation";
 
 
 function Verification(){
     const [user, setUser] = useState();
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
     const navigate = useNavigate();
     
     useEffect(() => {
@@ -30,11 +33,52 @@ function Verification(){
 
     }, []);
 
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        if(!password){
+            setError('Missing field!');
+        }
+
+        const userVerification = {
+            email: user.email,
+            password: password
+        };
+
+        const verificationResult = await fetch('/api/user/verify', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'Application/json',
+                'Authorization': `Bearer ${user.jwt}`
+            },
+            body: JSON.stringify(userVerification)
+        });
+
+        if(verificationResult.ok){
+            navigate('/product/upload');
+        } else{
+            const verificationResponse = await verificationResult.json();
+            setError(verificationResponse.errorMessage);
+        }
+
+    }
+
     return(
         
-        <div className="verification">
+        <div className="verification login-container">
             {user &&
-                <h1>Verification</h1>        
+                <>
+                    <h1 className="verification-title">Verification</h1>
+                    <UserAccountInformation user={user}/>
+                    <form className="login-form" onSubmit={handleSubmit}>
+                        <label htmlFor="password">Password: </label>
+                        <br />
+                        <input onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" className="password" value={password}/>
+                        <br />
+                        {error && <p className="error-message">{error}</p>}
+                        <button>Verify</button>
+                    </form>
+                </>
             }
         </div>
     )
