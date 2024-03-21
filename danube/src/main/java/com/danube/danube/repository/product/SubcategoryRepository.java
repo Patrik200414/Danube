cbc13f02e2281f2536d6099b47438e7e12228ed8
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-21 08:58:41 +0100

    Add findAllByCategory

--- danube/src/main/java/com/danube/danube/repository/product/SubcategoryRepository.java
+++ danube/src/main/java/com/danube/danube/repository/product/SubcategoryRepository.java
@@ -1,10 +1,13 @@
 package com.danube.danube.repository.product;
 
+import com.danube.danube.model.product.category.Category;
 import com.danube.danube.model.product.subcategory.Subcategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubcategoryRepository extends JpaRepository<Subcategory, Long> {
     Optional<Subcategory> findByName(String subcategoryName);
+    List<Subcategory> findAllByCategory(Category category);
 }
