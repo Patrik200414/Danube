a2d79e298bb9bf93538ba7b279e3da9c7abcfdc5
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-23 17:28:15 +0100

    Create NonExistingSubcategoryException

--- danube/src/main/java/com/danube/danube/custom_exception/product/NonExistingSubcategoryException.java
+++ danube/src/main/java/com/danube/danube/custom_exception/product/NonExistingSubcategoryException.java
@@ -1,2 +1,7 @@
-package com.danube.danube.custom_exception.product;public class NonExistingSubcategoryException {
+package com.danube.danube.custom_exception.product;
+
+public class NonExistingSubcategoryException extends RuntimeException{
+    public NonExistingSubcategoryException() {
+        super("The searched subcategory not exists!");
+    }
 }
