d3a65ea96f31323701fac8dad2d9fc3a39f26f24
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-24 18:42:39 +0100

    Implement saveProduct method

--- danube/src/main/java/com/danube/danube/service/ProductService.java
+++ danube/src/main/java/com/danube/danube/service/ProductService.java
@@ -1,22 +1,22 @@
 package com.danube.danube.service;
 
+import com.danube.danube.custom_exception.login_registration.NonExistingUserException;
+import com.danube.danube.custom_exception.product.NonExistingDetailException;
 import com.danube.danube.custom_exception.product.NonExistingProductCategoryException;
 import com.danube.danube.custom_exception.product.NonExistingSubcategoryException;
+import com.danube.danube.custom_exception.user.InvalidUserCredentialsException;
 import com.danube.danube.model.dto.product.*;
-/*import com.danube.danube.model.product.ProductDetail;
-import com.danube.danube.model.product.product_category.Category;
-import com.danube.danube.model.product.product_category.SubCategory;
-import com.danube.danube.model.product.product_information.ProductInformation;
-import com.danube.danube.repository.product.ProductDetailRepository;
-import com.danube.danube.repository.product.ProductInformationRepository;*/
 import com.danube.danube.model.product.Product;
 import com.danube.danube.model.product.category.Category;
+import com.danube.danube.model.product.connection.ProductValue;
 import com.danube.danube.model.product.detail.Detail;
 import com.danube.danube.model.product.subcategory.Subcategory;
-import com.danube.danube.repository.product.CategoryRepository;
-import com.danube.danube.repository.product.DetailRepository;
-import com.danube.danube.repository.product.ProductRepository;
-import com.danube.danube.repository.product.SubcategoryRepository;
+import com.danube.danube.model.product.value.Value;
+import com.danube.danube.model.user.Role;
+import com.danube.danube.model.user.UserEntity;
+import com.danube.danube.repository.product.*;
+import com.danube.danube.repository.product.connection.ProductValueRepository;
+import com.danube.danube.repository.user.UserRepository;
 import com.danube.danube.utility.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -31,14 +31,20 @@ public class ProductService {
     private final CategoryRepository categoryRepository;
     private final SubcategoryRepository subcategoryRepository;
     private final DetailRepository detailRepository;
+    private final UserRepository userRepository;
+    private final ValueRepository valueRepository;
+    private final ProductValueRepository productValueRepository;
     private final Converter converter;
 
     @Autowired
-    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository, SubcategoryRepository subcategoryRepository, DetailRepository detailRepository, Converter converter) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository, SubcategoryRepository subcategoryRepository, DetailRepository detailRepository, UserRepository userRepository, ValueRepository valueRepository, ProductValueRepository productValueRepository, Converter converter) {
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
         this.subcategoryRepository = subcategoryRepository;
         this.detailRepository = detailRepository;
+        this.userRepository = userRepository;
+        this.valueRepository = valueRepository;
+        this.productValueRepository = productValueRepository;
         this.converter = converter;
     }
 
@@ -105,16 +111,45 @@ public class ProductService {
     }
 
     public void saveProduct(ProductUploadDTO productUploadDTO){
-        /*
-        ProductInformation productInformation = productInformationCreator.createProduct(productUploadDTO.productInformation());
-        ProductDetail productDetail = converter.convertToProductDetail(productUploadDTO.productDetail());
+        UserEntity seller = userValidator(productUploadDTO.userId());
+        Product product = converter.convertProductDetailUploadDTOToProduct(
+                productUploadDTO.productDetail(), seller
+        );
 
-        productInformationRepository.save(productInformation);
-        productDetail.setProductInformation(productInformation);
-        productInformation.setProductDetail(productDetail);
+        productRepository.save(product);
+        Map<String, String> productInformation = productUploadDTO.productInformation();
+        saveProductValues(productInformation, product);
+    }
 
+    private void saveProductValues(Map<String, String> productInformation, Product product){
+        for(Map.Entry<String, String> entry : productInformation.entrySet()){
+            Detail detail = detailRepository.findByName(entry.getKey()).orElseThrow(
+                    () -> new NonExistingDetailException()
+            );
 
-        productDetailRepository.save(productDetail);
-        productInformationRepository.save(productInformation);*/
+            Value value = new Value();
+            value.setDetail(detail);
+            value.setValue(entry.getValue());
+
+            Value savedValue = valueRepository.save(value);
+            ProductValue productValue = new ProductValue();
+            productValue.setProduct(product);
+            productValue.setValue(savedValue);
+        }
+    }
+
+    private UserEntity userValidator(long userId){
+        Optional<UserEntity> searchedSeller = userRepository.findById(userId);
+
+        if(searchedSeller.isEmpty()){
+            throw new NonExistingUserException();
+        }
+
+        UserEntity user = searchedSeller.get();
+        if(!user.getRoles().contains(Role.ROLE_SELLER)){
+            throw new InvalidUserCredentialsException();
+        }
+
+        return user;
     }
 }
