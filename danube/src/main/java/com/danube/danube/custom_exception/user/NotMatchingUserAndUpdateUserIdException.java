2bffe119951b3dc003219b5cfb54eb8909b4cbe3
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-03-31 14:15:38 +0200

    Create NotMatchingUserAndUpdateUserIdException

--- danube/src/main/java/com/danube/danube/custom_exception/user/NotMatchingUserAndUpdateUserIdException.java
+++ danube/src/main/java/com/danube/danube/custom_exception/user/NotMatchingUserAndUpdateUserIdException.java
@@ -1,2 +1,7 @@
-package com.danube.danube.custom_exception.user;public class NotMatchingUserAndUpdateUserIdException {
+package com.danube.danube.custom_exception.user;
+
+public class NotMatchingUserAndUpdateUserIdException extends RuntimeException{
+    public NotMatchingUserAndUpdateUserIdException() {
+        super("Not matching user and update user id!");
+    }
 }
