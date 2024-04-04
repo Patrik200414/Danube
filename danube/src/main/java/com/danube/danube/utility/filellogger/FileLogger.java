39128a00cfe35735d6ab502cfef109e8e27d5e8c
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-04 17:06:09 +0200

    Implement saving file and file stream writeing

--- danube/src/main/java/com/danube/danube/utility/filellogger/FileLogger.java
+++ danube/src/main/java/com/danube/danube/utility/filellogger/FileLogger.java
@@ -1,2 +1,23 @@
-package com.danube.danube.utility.filellogger;public class FileLogger {
+package com.danube.danube.utility.filellogger;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+abstract public class FileLogger {
+    private static final String BASE_PATH = String.format("%s\\src\\main\\resources\\images\\", System.getProperty("user.dir"));
+    public static void saveFile(MultipartFile[] multipartFile) throws IOException {
+
+        for(MultipartFile file : multipartFile){
+            System.out.println(file.getContentType());
+
+            writeToFile(file.getBytes(), file.getOriginalFilename());
+        }
+    }
+
+    private static void writeToFile(byte[] image, String path) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(BASE_PATH + path);
+        fileOutputStream.write(image);
+    }
 }
