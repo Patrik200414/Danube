31c17c489f25138b622e6db573e151cf44e0f1c7
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-08 15:08:28 +0200

    Add ProductContainer to Home component

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../component/NavBar";
+import ProductContainer from "../component/ProductContainer";
 
 function Home(){
     return(
         <div className="home">
             <NavBar />
+            <ProductContainer />
         </div>
     )
 }
