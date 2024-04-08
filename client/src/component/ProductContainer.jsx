8e136c715910a4e5dc52c07b6390955aa0d4df4c
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-08 16:40:55 +0200

    Refactor ProductContainer component and replace fetch to Home

--- client/src/component/ProductContainer.jsx
+++ client/src/component/ProductContainer.jsx
@@ -1,35 +1,16 @@
-import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import Proptypes from "prop-types";
 
-function ProductContainer(){
-    const [products, setProducts] = useState();
-    const [itemPerPage, setItemPerPage] = useState(9);
-    const [pageNumber, setPageNumber] = useState(0);
-
-    const [error, setError] = useState();
-
-    useEffect(() => {
-        const getProducts = async () => {
-            const productsData = await fetch(`/api/product?pageNumber=${pageNumber}&itemPerPage=${itemPerPage}`);
-            const productResponse = await productsData.json();
-            console.log(productResponse);
-            if(productsData.ok){
-                setProducts(productResponse);
-                setError();
-            } else{
-                setError(productResponse.errorMessage);
-            }
-        }
-
-        getProducts();
-    }, [itemPerPage, pageNumber])
-
+function ProductContainer({products}){
     return(
         <div className="product-container">
-            {error && <p className="error-message">{error}</p>}
             {products && products.map(product => <ProductCard key={product.id} product={product}/>)}
         </div>
     )
+}
+
+ProductContainer.propTypes = {
+    products: Proptypes.array
 }
 
 export default ProductContainer;
