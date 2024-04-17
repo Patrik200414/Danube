f7e950eaa056b9ad60f4be4e944e850ff55dc329
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-17 15:49:43 +0200

    Render product image

--- client/src/component/ProductCard.jsx
+++ client/src/component/ProductCard.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import defaultProductImage from '../static/defaultProduct.jpg';
 import Proptypes from 'prop-types';
 
 function ProductCard({product}){
+    const [imageNumber, setImageNumber] = useState(0);
+
+    function calculateImageSourceName(imageName){
+        return `http://localhost:8080/images/${imageName}`;
+    }
+
     return(
         <div className="product-card">
-            <img src={defaultProductImage}/>
+            <img src={product.images.length ? calculateImageSourceName(product.images[imageNumber]) : defaultProductImage}/>
             <h3>{product.productName}</h3>
             <h4>US ${product.price}</h4>
             <div className="sub-informations">
