4764979d479efe95c55c90ab3b9e6e59a45db64c
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-19 10:04:44 +0200

    Refactor entity to join with SubcategoryDetail entity

--- danube/src/main/java/com/danube/danube/model/product/detail/Detail.java
+++ danube/src/main/java/com/danube/danube/model/product/detail/Detail.java
@@ -1,6 +1,6 @@
 package com.danube.danube.model.product.detail;
 
-import com.danube.danube.model.product.subcategory.Subcategory;
+import com.danube.danube.model.product.connection.SubcategoryDetail;
 import com.danube.danube.model.product.value.Value;
 import jakarta.persistence.*;
 
@@ -11,22 +11,23 @@ public class Detail {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
-    @ManyToOne
-    private Subcategory subcategory;
+    @OneToMany(mappedBy = "detail")
+    private List<SubcategoryDetail> subcategoryDetails;
     @OneToMany(mappedBy = "detail")
     private List<Value> values;
 
     public Detail() {
     }
 
-    public Detail(long id, String name, Subcategory subcategory, List<Value> values) {
+    public Detail(long id, String name, List<SubcategoryDetail> subcategoryDetails, List<Value> values) {
         this.id = id;
         this.name = name;
-        this.subcategory = subcategory;
+        this.subcategoryDetails = subcategoryDetails;
         this.values = values;
     }
+
 
     public long getId() {
         return id;
@@ -44,12 +45,12 @@ public class Detail {
         this.name = name;
     }
 
-    public Subcategory getSubcategory() {
-        return subcategory;
+    public List<SubcategoryDetail> getSubcategoryDetails() {
+        return subcategoryDetails;
     }
 
-    public void setSubcategory(Subcategory subcategory) {
-        this.subcategory = subcategory;
+    public void setSubcategoryDetails(List<SubcategoryDetail> subcategoryDetails) {
+        this.subcategoryDetails = subcategoryDetails;
     }
 
     public List<Value> getValues() {
