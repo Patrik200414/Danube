12d35fb1c77d1230b6d2f69b7109f4c29f6806c8
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-23 15:44:20 +0200

    Create ProductItemDTO

--- danube/src/main/java/com/danube/danube/model/dto/product/ProductItemDTO.java
+++ danube/src/main/java/com/danube/danube/model/dto/product/ProductItemDTO.java
@@ -1,2 +1,19 @@
-package com.danube.danube.model.dto.product;public class ProductItemDTO {
+package com.danube.danube.model.dto.product;
+
+import java.util.List;
+
+public record ProductItemDTO(
+        String productName,
+        double price,
+        int deliveryTimeInDay,
+        int quantity,
+        int rating,
+        double shippingPrice,
+        int sold,
+        String brand,
+        String description,
+        String seller,
+        List<String> images,
+        List<DetailValueDTO> detailValues
+) {
 }
