dad2b337f568c9603319dd8bbd45ffbba6961015
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-25 12:16:20 +0200

    Create NotFound page

---
+++ client/src/page/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound(){
+    return(
+        <div className="not-found-container">
+            <h1>404! Unfortunatelly the searched page is not found!</h1>
+        </div>
+    )
+}
+
+export default NotFound;
