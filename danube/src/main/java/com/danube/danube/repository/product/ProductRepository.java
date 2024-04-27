0caee3614ade0c2a9e5a7f2b268c3df8bff200d3
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-27 09:57:32 +0200

    Create query method

--- danube/src/main/java/com/danube/danube/repository/product/ProductRepository.java
+++ danube/src/main/java/com/danube/danube/repository/product/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.danube.danube.repository.product;
 
 import com.danube.danube.model.product.Product;
+import com.danube.danube.model.product.subcategory.Subcategory;
 import com.danube.danube.model.user.UserEntity;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,4 +12,5 @@ import java.util.Set;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findBySeller(UserEntity seller);
+    List<Product> findBySubcategoryAndIdNotOrderBySoldDescRatingDesc(Subcategory subcategory, long id, Pageable pageable);
 }
