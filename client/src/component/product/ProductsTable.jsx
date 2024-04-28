45e795b71312b9f66b7c8f065fd63fe4f83a9596
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-28 20:31:17 +0200

    Create product update table

--- client/src/component/product/ProductsTable.jsx
+++ client/src/component/product/ProductsTable.jsx
@@ -1,46 +1,42 @@
 import { Link } from "react-router-dom";
+import Proptypes from 'prop-types';
+
+import calculateImageSourceName from "../../utility/calculateImageSourceName";
 
 function ProductsTable({products, buttons}){
+
+
     return(
-        <table>
+        <table className="products-table">
             <thead>
                 <tr>
-                    {Object.keys(products[0]).map(fieldName => <th key={fieldName}>{fieldName}</th>)}
+                    {Object.keys(products[0]).map(fieldName => fieldName !== 'id' ? <th key={fieldName}>{fieldName}</th> : '')}
                     {buttons.map(button => <th key={button.actionName}>{button.actionName}</th>)}
                 </tr>
             </thead>
-        <tbody>
-            {products.map(product => {
-                return (
-                    <tr key={product.id}>
-                        {Object.keys(product).map(fieldName => <td key={`${fieldName}-${product.id}`}>{product[fieldName]}</td>)}
-                        {buttons.map(button => (
-                            <Link key={`${button.actionName}-${product.id}`} to={button.isDynamicLink ? `${button.linkTo}/${product.id}` : button.linkTo}>
-                                <button onClick={() => button.onClick}>{button.buttonText}</button>
-                            </Link>
-                        ))}
-                    </tr>
-                )
-            })}
-
-            {/* {employees.map((employee) => (
-            <tr key={employee._id}>
-                <td>{employee.name}</td>
-                <td>{employee.level}</td>
-                <td>{employee.position}</td>
-                <td>
-                <Link to={`/update/${employee._id}`}>
-                    <button type="button">Update</button>
-                </Link>
-                <button type="button" onClick={() => onDelete(employee._id)}>
-                    Delete
-                </button>
-                </td>
-            </tr>
-            ))} */}
-        </tbody>
+            <tbody>
+                {products.map(product => {
+                    return (
+                        <tr key={product.id}>
+                            {Object.keys(product).map(fieldName => fieldName !== 'id' ? <td key={`${fieldName}-${product.id}`}> {fieldName.toLocaleLowerCase().includes('image') ? <img className="my-product-image" src={calculateImageSourceName(product[fieldName])}/> : product[fieldName]}</td> : '')}
+                            {buttons.map(button => (
+                                <td key={`${button.actionName}-${product.id}`}>
+                                    <Link to={button.isDynamic && button.linkTo ? `${button.linkTo}/${product.id}` : button.linkTo}>
+                                        <button onClick={button.onClick ? button.isDynamic ? () => button.onClick(product.id) : () => button.onClick() : () => ''}>{button.buttonText}</button>
+                                    </Link>
+                                </td>
+                            ))}
+                        </tr>
+                    )
+                })}
+            </tbody>
         </table>
     )
+}
+
+ProductsTable.propTypes = {
+    products: Proptypes.array,
+    buttons: Proptypes.array
 }
 
 export default ProductsTable;
