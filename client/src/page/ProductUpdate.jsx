4d01ddffb69f4a903625a25f707b69099762d436
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-29 13:26:44 +0200

    Create handle change functions

--- client/src/page/ProductUpdate.jsx
+++ client/src/page/ProductUpdate.jsx
@@ -3,20 +3,43 @@ import useFetch from "../utility/customHook/useFetch";
 import useVerifySeller from "../utility/customHook/useVerifySeller";
 import ProductInformationForm from "../component/product/ProductInformationForm";
 import ProductDetailsForm from "../component/product/ProductDetailsForm";
+import changeProductDetail from '../utility/changeProductDetail';
 
 function ProductUpdate(){
     const {productId} = useParams();
 
     const [user] = useVerifySeller();
-    const [product] = useFetch(`/api/product/item/${productId}`);
+    const [product, setProduct] = useFetch(`/api/product/item/${productId}`);
+
+    function handleProductInformationChange(value, key){
+        const updatedProduct = {
+            ...product
+        };
+        updatedProduct.productInformation[key] = value;
+        setProduct(updatedProduct);
+    }
+
+    function handleDetailChange(value, detailId){
+        const copiedProductDetails = [...product.detailValues];
+        const updatedDetails = changeProductDetail(value, detailId, copiedProductDetails);
+        const updatedProduct = {
+            ...product,
+            detailValues: [...updatedDetails]
+        }
+
+        setProduct(updatedProduct);
+    }
+
+
 
     
     return(
         <div className="product-update-container">
             {product &&
-                <div>
-                    <ProductInformationForm onDetailsChange={() => console.log('asd')} productDetail={product.productInformation}/>
-                    <ProductDetailsForm details={product.detailValues} onDetailsChange={() => console.log('asd')} subCategoryId={1} user={user} onDetailsSet={() => console.log('asd')} onImageUpload={() => console.log('asd')}/>
+                <div className="product-upload">
+                    <ProductInformationForm onDetailsChange={(value, key) => handleProductInformationChange(value, key)} productDetail={product.productInformation}/>
+                    <ProductDetailsForm details={product.detailValues} onDetailsChange={(value, key) => handleDetailChange(value, key)} subCategoryId={product.productInformation.subcategoryId} user={user} onDetailsSet={() => console.log('asd')} onImageUpload={() => console.log('asd')}/>
+                    <button type="submit">Save</button>
                 </div>
             }
         </div>
