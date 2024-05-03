dfca8c2b83cd07dcb867730dc65208e86643434c
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-05-03 13:21:00 +0200

    Rename custom hook

---
+++ client/src/utility/customHook/useVerifyUserRole.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function useVerifyUserRole(role){
+    const [user, setUser] = useState();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const currUser = sessionStorage.getItem('USER_JWT');
+
+        if(!currUser){
+            navigate('/');
+            return;
+        }
+
+        const userData = JSON.parse(currUser);
+        if(!userData.roles.includes(role)){
+            navigate('/');
+            return
+        }
+
+        setUser(userData);
+    }, [navigate, role]);
+
+    return [user];
+}
+
+
+export default useVerifyUserRole;
