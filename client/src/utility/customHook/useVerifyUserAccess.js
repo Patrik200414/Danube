55cfddbba890234cadfc872137423f0914d7f8b4
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-05-03 13:21:36 +0200

    Create custom hook for verify user access

---
+++ client/src/utility/customHook/useVerifyUserAccess.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function useVerifyUserAccess(navigateUrl){
+    const [isVerifed, setIsVerified] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const verifyUser = async () => {
+            const userJwt = JSON.parse(sessionStorage.getItem('USER_JWT'))
+            const verifyData = await fetch('/api/user/verify', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': `Bearer ${userJwt.jwt}`
+                },
+                body: JSON.stringify({token: userJwt.jwt})
+            });
+            
+            if(!verifyData.ok){
+                navigate(navigateUrl);
+                return 
+            }
+            setIsVerified(true);
+        }
+
+        verifyUser();
+
+    }, [navigateUrl, navigate])
+
+    return isVerifed;
+}
+
+export default useVerifyUserAccess;
