ba386c2435c8d7f525209b663266293c7abc4b26
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-05-04 16:08:19 +0200

    Create cart endpoint

--- danube/src/main/java/com/danube/danube/controller/OrderController.java
+++ danube/src/main/java/com/danube/danube/controller/OrderController.java
@@ -3,9 +3,14 @@ package com.danube.danube.controller;
 import com.danube.danube.model.dto.order.AddToCartDTO;
 import com.danube.danube.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class OrderController {
     private final OrderService orderService;
 
@@ -14,7 +19,9 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    public void addToCart(AddToCartDTO cartItem){
-
+    @PostMapping("/cart")
+    public HttpStatus addToCart(@RequestBody AddToCartDTO cartItem){
+        orderService.addToCart(cartItem);
+        return HttpStatus.CREATED;
     }
 }
